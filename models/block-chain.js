7f954e472a1c23cb2313f82d3e9c7569bc8a7554
trasanthanh <44344701+trasanthanh@users.noreply.github.com>
2020-06-01 00:22:52 +0700

    fix caculator balance

--- models/block-chain.js
+++ models/block-chain.js
@@ -51,10 +51,10 @@ class Blockchain{
         for(const block of this.chain){
             for(const trans of block.transactions){
                 if (trans.fromAddress === address){
-                    balance -= trans.amount;
+                    balance -= parseInt(trans.amount);
                 }
                 if(trans.toAddress === address){
-                    balance += trans.amount;
+                    balance += parseInt(trans.amount);
                 }
             }
         }
